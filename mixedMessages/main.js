11293cc4c9667d23b38d6668bf5ea15baf1323b5
hongbre <hongbre@gmail.com>
2025-12-09 22:49:55 +0900

    mixedMessages final commit

--- mixedMessages/main.js
+++ mixedMessages/main.js
@@ -1,10 +1,16 @@
+const { setTimeout: sleep } = require('timers/promises');
+
 const messages = [
     'Hello, World!',
     'How are you today?',
     'Goodbye!'
 ];
 
-const randomIndex = Math.floor(Math.random() * messages.length);
-const randomMessage = messages[randomIndex];
+const main = async () => {
+    while (true) {
+        console.log(messages[Math.floor(Math.random() * messages.length)]);
+        await sleep(1000);
+    }
+};
 
-console.log(randomMessage);
+main();
