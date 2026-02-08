fc4b9e3779b6bd0358d475a4b0184cca7936b69c
hongbre <hongbre@gmail.com>
2026-02-08 14:21:14 +0900

    Attributes In JSX

--- ReactPart1/IntroToJSX/app.js
+++ ReactPart1/IntroToJSX/app.js
@@ -1,7 +1,17 @@
-// const h1 = <h1>Hello world</h1>;
+// JSX Elements
+// 1. In 'app.js', write a JSX '<p>' element containing the text, 'Hello world'.
+const pHelloWorld = <p>Hello world</p>;
 
-// In 'app.js', write a JSX '<p>' element containing the text, 'Hello world'.
-// const pHelloWorld = <p>Hello world</p>;
-
-// Create a JSX <article> element. Save it in a variable named myArticle.
+// JSX Elements And Their Surroundings
+// 1. Create a JSX <article> element. Save it in a variable named myArticle.
 const myArticle = <article></article>;
+
+// Attributes In JSX
+// 1. Declare a constant named 'p1'. 
+// Set 'p1' equal to a JSX '<p>' element containing the text 'foo'.
+// 2. On the next line, declare a constant named 'p2'.
+// Set 'p2' equal to another JSX '<p>' element containing the word 'bar'.
+// 3. Give the '<p>' stored in 'p1' an 'id' attribute of 'large'.
+// Give the '<p>' stored in 'p2' an 'id' attribute of 'small'.
+const p1 = <p id='large'>foo</p>;
+const p2 = <p id='small'>bar</p>;
