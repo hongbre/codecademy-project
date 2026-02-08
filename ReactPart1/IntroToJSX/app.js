72ef44364ed69848130e9581557f62fc589d6723
hongbre <hongbre@gmail.com>
2026-02-08 14:15:45 +0900

    JSX Elements And Their Surroundings

--- ReactPart1/IntroToJSX/app.js
+++ ReactPart1/IntroToJSX/app.js
@@ -1,4 +1,7 @@
 // const h1 = <h1>Hello world</h1>;
 
 // In 'app.js', write a JSX '<p>' element containing the text, 'Hello world'.
-const pHelloWorld = <p>Hello world</p>;
+// const pHelloWorld = <p>Hello world</p>;
+
+// Create a JSX <article> element. Save it in a variable named myArticle.
+const myArticle = <article></article>;
