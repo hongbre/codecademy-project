d9baed446fd98ecd86f47dbd8a3928b0af6c9524
hongbre <hongbre@gmail.com>
2026-02-08 14:13:37 +0900

    JSX Elements

--- ReactPart1/IntroToJSX/app.js
+++ ReactPart1/IntroToJSX/app.js
@@ -1,1 +1,4 @@
-const h1 = <h1>Hello world</h1>;
+// const h1 = <h1>Hello world</h1>;
+
+// In 'app.js', write a JSX '<p>' element containing the text, 'Hello world'.
+const pHelloWorld = <p>Hello world</p>;
