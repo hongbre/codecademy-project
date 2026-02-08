3478c0916c1b08b1046434f92e26fbea7766d0f6
hongbre <hongbre@gmail.com>
2026-02-08 14:29:18 +0900

    JSX Outer Elements

--- ReactPart1/IntroToJSX/app.js
+++ ReactPart1/IntroToJSX/app.js
@@ -27,3 +27,32 @@ const myDiv = ( // element를 중첩시킬 때는 ()로 묶어줘야 한다.
     </h1>
   </div>
 );
+
+// JSX Outer Elements
+// 1. Your friend’s blog is down! He’s asked you to fix it. 
+// You immediately diagnose the problem: a JSX expression with multiple outer elements.
+// Repair your friend’s broken code by wrapping the JSX in a '<div>' element.
+
+/* Before
+const blog = (
+  <img src="pics/192940u73.jpg" />
+  <h1>
+    Welcome to Dan's Blog!
+  </h1>
+  <article>
+    Wow I had the tastiest sandwich today. I <strong>literally</strong> almost freaked out.
+  </article>
+);
+*/
+
+const blog = (
+  <div>
+  	<img src="pics/192940u73.jpg" />
+  	<h1>
+    	Welcome to Dan's Blog!
+  	</h1>
+  	<article>
+    	Wow I had the tastiest sandwich today. I <strong>literally</strong> almost freaked out.
+  	</article>    
+  </div>
+);
